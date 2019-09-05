a809a8e92bcb16e9a13c36ffc1b3f482d4e30b81
Maria Cristina <mcfeijoo2019@gmail.com>
2019-09-05 15:23:25 -0500

    Drag & Drop finalizado

--- js/app.js
+++ js/app.js
@@ -33,122 +33,196 @@ function CargarTableroYDesaparecerElementos(){
   // Si el tablero estuvo incompleto, es decir, se agregó al menos un elemento, se hará el metodo para desaparecer elementos y se llamará nuevamente al metodo de cargar
   if (!tableroCompleto){
       // Si hay 3 o mas elementos, se hace el efecto y se desaparecen
-      for (var i=0;i<7;i++){
-        for (var j=0;j<7;j++){
-          ValidaSiHayElementosIguales(i,j);
-        }
-      }
+      RecorreTableroParaVerificacion()
       // Se realiza el efecto y desaparecen los elementos
       // EfectoDulce();
       // Se llama nuevamente al metodo recursivamente porque se han eliminado elementos, debe volver a hacerse la validación
       // CargarTableroYDesaparecerElementos()
   }
 }
-
+function RecorreTableroParaVerificacion(){
+  for (var i=0;i<7;i++){
+    for (var j=0;j<7;j++){
+      ValidaSiHayElementosIguales(i,j);
+    }
+  }
+}
 function ValidaSiHayElementosIguales(i,j){
-  if (TieneMasDe2ElementosIgualesArriba(i,j)){
+  var padre=$('.col-'+(i+1));
+  var numImg=$(padre.children()[j]).attr('src').substr(6,1);
+  if (TieneMasDe2ElementosIgualesArriba(padre,numImg,i,j,'arr')){
     // Cambia la clase y el de arriba ya no recorre las otras posiciones
     $($('.col-'+(i+1)).children()[j]).addClass('elimina');
     $($('.col-'+(i+1)).children()[j-1]).addClass('elimina');
   }
-  else if(TieneMasDe2ElementosIgualesAbajo(i,j)){
+  else if(TieneMasDe2ElementosIgualesAbajo(padre,numImg,i,j,'aba')){
     // Cambia la clase y el de abajo ya no recorre las otras posiciones
     $($('.col-'+(i+1)).children()[j]).addClass('elimina');
     $($('.col-'+(i+1)).children()[j+1]).addClass('elimina');
   }
-  else if(TieneMasDe2ElementosIgualesDerecha(i,j)){
+  else if(TieneMasDe2ElementosIgualesDerecha(padre,numImg,i,j,'der')){
     // Cambia la clase y el de la derecha ya no recorre las otras posiciones
     $($('.col-'+(i+1)).children()[j]).addClass('elimina');
     $($('.col-'+(i+2)).children()[j]).addClass('elimina');
   }
-  else if(TieneMasDe2ElementosIgualesIzquierda(i,j)){
+  else if(TieneMasDe2ElementosIgualesIzquierda(padre,numImg,i,j,'izq')){
     // Cambia la clase y el de la izquierda ya no recorre las otras posiciones
     $($('.col-'+(i+1)).children()[j]).addClass('elimina');
     $($('.col-'+(i)).children()[j]).addClass('elimina');
   }
 }
 
-function TieneMasDe2ElementosIgualesArriba(colu,fil){
-  if(fil>=2){
-    var padre=$('.col-'+(colu+1));
-    var numImg=$(padre.children()[fil]).attr('src').substr(6,1);
-    var numRepeticiones=1;
-    for(var i=1;i<3;i++){
-      var numImgHermano=$(padre.children()[fil-i]).attr('src').substr(6,1);
-      if (numImg==numImgHermano){
-          numRepeticiones+=1;
+function TieneMasDe2ElementosIgualesArriba(padre,numImg,colu,fil,posicion){
+  if(posicion!='aba'){
+    if(fil>=2){
+      var numRepeticiones=1;
+      for(var i=1;i<3;i++){
+        var numImgHermano=$(padre.children()[fil-i]).attr('src').substr(6,1);
+        if (numImg==numImgHermano){
+            numRepeticiones+=1;
+        }
+        else{break;}
       }
-      else{break;}
-    }
-    if(numRepeticiones==3){
-      console.log('Arr='+colu,fil)
-      return true;
+      if(numRepeticiones==3){
+        return true;
+      }
+      else{return false;}
     }
     else{return false;}
   }
-  else{return false;}
 }
-function TieneMasDe2ElementosIgualesAbajo(colu,fil){
-  if(fil<=4){
-    var padre=$('.col-'+(colu+1));
-    var numImg=$(padre.children()[fil]).attr('src').substr(6,1);
-    var numRepeticiones=1;
-    for(var i=1;i<3;i++){
-      var numImgHermano=$(padre.children()[fil+i]).attr('src').substr(6,1);
-      if (numImg==numImgHermano){
-          numRepeticiones+=1;
+function TieneMasDe2ElementosIgualesAbajo(padre,numImg,colu,fil,posicion){
+  if(posicion!='arr'){
+    if(fil<=4){
+      var numRepeticiones=1;
+      for(var i=1;i<3;i++){
+        var numImgHermano=$(padre.children()[fil+i]).attr('src').substr(6,1);
+        if (numImg==numImgHermano){
+            numRepeticiones+=1;
+        }
+        else{break;}
       }
-      else{break;}
-    }
-    if(numRepeticiones==3){
-      console.log('Aba='+colu,fil)
-      return true;
+      if(numRepeticiones==3){
+        return true;
+      }
+      else{return false;}
     }
     else{return false;}
   }
-  else{return false;}
 }
-function TieneMasDe2ElementosIgualesDerecha(colu,fil){
-  if(colu<=4){
-    var padre=$('.col-'+(colu+1));
-    var numImg=$(padre.children()[fil]).attr('src').substr(6,1);
-    var numRepeticiones=1;
-    for(var i=1;i<3;i++){
-      var sgtePadre=$('.col-'+(colu+1+i))
-      var numImgHermano=$(sgtePadre.children()[fil]).attr('src').substr(6,1);
-      if (numImg==numImgHermano){
-          numRepeticiones+=1;
+function TieneMasDe2ElementosIgualesDerecha(padre,numImg,colu,fil,posicion){
+  if(posicion!='izq'){
+    if(colu<=4){
+      var numRepeticiones=1;
+      for(var i=1;i<3;i++){
+        var sgtePadre=$('.col-'+(colu+1+i));
+        var numImgHermano=$(sgtePadre.children()[fil]).attr('src').substr(6,1);
+        if (numImg==numImgHermano){
+            numRepeticiones+=1;
+        }
+        else{break;}
       }
-      else{break;}
-    }
-    if(numRepeticiones==3){
-      console.log('Der='+colu,fil)
-      return true;
+      if(numRepeticiones==3){
+        return true;
+      }
+      else{return false;}
     }
     else{return false;}
   }
-  else{return false;}
 }
-function TieneMasDe2ElementosIgualesIzquierda(colu,fil){
-  if(colu>=2){
-    var padre=$('.col-'+(colu+1));
-    var numImg=$(padre.children()[fil]).attr('src').substr(6,1);
-    var numRepeticiones=1;
-    for(var i=1;i<3;i++){
-      var sgtePadre=$('.col-'+(colu+1-i))
-      var numImgHermano=$(sgtePadre.children()[fil]).attr('src').substr(6,1);
-      if (numImg==numImgHermano){
-          numRepeticiones+=1;
+function TieneMasDe2ElementosIgualesIzquierda(padre,numImg,colu,fil,posicion){
+  if(posicion!='der'){
+    if(colu>=2){
+      var numRepeticiones=1;
+      for(var i=1;i<3;i++){
+        var sgtePadre=$('.col-'+(colu+1-i));
+        var numImgHermano=$(sgtePadre.children()[fil]).attr('src').substr(6,1);
+        if (numImg==numImgHermano){
+            numRepeticiones+=1;
+        }
+        else{break;}
       }
-      else{break;}
-    }
-    if(numRepeticiones==3){
-      console.log('Izq='+colu,fil)
-      return true;
+      if(numRepeticiones==3){
+        return true;
+      }
+      else{return false;}
     }
     else{return false;}
   }
-  else{return false;}
+}
+function EsElementoCentral(padre,numImg,colu,fil,posicion){
+  // Si tiene al menos un elemento arriba y abajo o a la derecha e izquierda, es un elemento central
+  if(posicion=='der' || posicion=='izq'){//Solo evalua arriba y abajo
+    if(fil>=1 && fil<=5){
+      var numImgArr=$(padre.children()[fil-1]).attr('src').substr(6,1);
+      var numImgAba=$(padre.children()[fil+1]).attr('src').substr(6,1);
+      if(numImg==numImgArr && numImg==numImgAba){
+        return true;
+      }
+      else{return false;}
+    }
+    else{return false;}
+  }
+  else{//Solo evalua derecha o izquierda
+    if(colu>=1 && colu<=5){
+      var sgtePadre=$('.col-'+(colu+1));
+      var antPadre=$('.col-'+(colu-1));
+      var numImgDer=$(sgtePadre.children()[fil]).attr('src').substr(6,1);
+      var numImgIzq=$(antPadre.children()[fil]).attr('src').substr(6,1);
+      if(numImg==numImgDer && numImg==numImgIzq){
+        return true;
+      }
+      else{return false;}
+    }
+    else{return false;}
+  }
+}
+function MoverElemento(filArrastrada,colArrastrada,imgArrastrada,filContenedor,colContenedor,imgContenedor){
+  // Se valida que el elemento pueda ser movido
+  var nuevaPosArrastra,nuevaPosContenedor;
+  var mover=false;
+
+  if(colArrastrada!=colContenedor){//Se mueve a la derecha o izquierda
+    if(colArrastrada<colContenedor){//Arrastra a la derecha
+      nuevaPosArrastra='der';
+      nuevaPosContenedor='izq';
+    }
+    else{//Arrastra a la izquierda
+      nuevaPosArrastra='izq';
+      nuevaPosContenedor='der';
+    }
+  }
+  else{//Se mueve arriba o abajo
+    if(filArrastrada<filContenedor){//Arrastra abajo
+      nuevaPosArrastra='aba';
+      nuevaPosContenedor='arr';
+    }
+    else{//Arrastra arriba
+      nuevaPosArrastra='arr';
+      nuevaPosContenedor='aba';
+    }
+  }
+
+  var padreArr=$('.col-'+(colContenedor+1));
+  var numImgArr=imgArrastrada.substr(6,1);
+  var padreCont=$('.col-'+(colArrastrada+1));
+  var numImgCont=imgContenedor.substr(6,1);
+
+  if((TieneMasDe2ElementosIgualesArriba(padreArr,numImgArr,colContenedor,filContenedor,nuevaPosArrastra) ||
+        TieneMasDe2ElementosIgualesAbajo(padreArr,numImgArr,colContenedor,filContenedor,nuevaPosArrastra) ||
+          TieneMasDe2ElementosIgualesIzquierda(padreArr,numImgArr,colContenedor,filContenedor,nuevaPosArrastra) ||
+              TieneMasDe2ElementosIgualesDerecha(padreArr,numImgArr,colContenedor,filContenedor,nuevaPosArrastra) ||
+                EsElementoCentral(padreArr,numImgArr,colContenedor,filContenedor,nuevaPosArrastra))
+    ||
+    (TieneMasDe2ElementosIgualesArriba(padreCont,numImgCont,colArrastrada,filArrastrada,nuevaPosContenedor) ||
+        TieneMasDe2ElementosIgualesAbajo(padreCont,numImgCont,colArrastrada,filArrastrada,nuevaPosContenedor) ||
+            TieneMasDe2ElementosIgualesIzquierda(padreCont,numImgCont,colArrastrada,filArrastrada,nuevaPosContenedor) ||
+                TieneMasDe2ElementosIgualesDerecha(padreCont,numImgCont,colArrastrada,filArrastrada,nuevaPosContenedor) ||
+                  EsElementoCentral(padreCont,numImgCont,colArrastrada,filArrastrada,nuevaPosContenedor))){
+    mover=true;
+  }
+
+  return mover;
 }
 function EfectoDulce(){
   $('.elimina').fadeToggle('fast',function(){
@@ -177,17 +251,18 @@ $(function(){
         filContenedor=$(this).index();
         colContenedor=parseInt($(this).parent().attr('class').substr(4,1));
         // Se hace la verificacion para que el elemento pueda ser movido o vuelva a su posicion
-        if(((TieneMasDe2ElementosIgualesArriba(colArrastrada-1,filArrastrada-1)) ||
-              (TieneMasDe2ElementosIgualesAbajo(colArrastrada-1,filArrastrada+1)) ||
-                (TieneMasDe2ElementosIgualesIzquierda(colArrastrada-2,filArrastrada)) ||
-                    (TieneMasDe2ElementosIgualesDerecha(colArrastrada,filArrastrada)))
-            ||
-            ((TieneMasDe2ElementosIgualesArriba(colContenedor-1,filContenedor-1)) ||
-                  (TieneMasDe2ElementosIgualesAbajo(colContenedor-1,filContenedor+1)) ||
-                    (TieneMasDe2ElementosIgualesIzquierda(colContenedor-2,filContenedor)) ||
-                        (TieneMasDe2ElementosIgualesDerecha(colContenedor,filContenedor)))){
-          ui.draggable.position({of: $(this),my: 'left top',at: 'left top'});
-          ui.draggable.draggable('option', 'revert', "invalid");
+        if((Math.abs(filArrastrada-filContenedor)<=1) && (Math.abs(colArrastrada-colContenedor)<=1) &&
+            (imgArrastrada!=imgContenedor) && ((filArrastrada==filContenedor) || (colArrastrada==colContenedor))){
+          if(MoverElemento(filArrastrada,colArrastrada-1,imgArrastrada,filContenedor,colContenedor-1,imgContenedor)){
+            ui.draggable.position({of: $(this),my: 'left top',at: 'left top'});
+            ui.draggable.draggable('option', 'revert', "invalid");
+            // Se modifican los atributos src de los elementos movidos en el DOM
+            $($('.col-'+colArrastrada).children()[filArrastrada]).attr('src',imgContenedor);
+            $($('.col-'+colContenedor).children()[filContenedor]).attr('src',imgArrastrada);
+          }
+          else{
+            ui.draggable.draggable('option', 'revert', "valid");
+          }
         }
         else{
           ui.draggable.draggable('option', 'revert', "valid");
@@ -195,5 +270,3 @@ $(function(){
       }
     })
 })
-// puedes modificarlos cambiando el atributo src de ambos
-// al hacer click obtener la img del item que se va a arrastrar
